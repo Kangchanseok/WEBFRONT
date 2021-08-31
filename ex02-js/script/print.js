30bac1a1283de5019c802c434abe8a4bcc5341be
강찬석 <ssimjunlke@naver.com>
2021-08-31 17:16:33 +0900

    ex02

---
+++ ex02-js/script/print.js
@@ -0,0 +1,9 @@
+  
+        if(name === null){
+            name = "Anonumous";
+        }
+        document.writeln("<p>Welcome, " + name + "<p>");
+            let result = confirm("준비되셨나용??"); // 사용자 선택
+            if (result){
+                alert("한번 실행해 봅시다!");
+            }
